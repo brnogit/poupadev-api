3a0f6302258a36bd07a725515a5a75975098a63a
bmotadev <bmotadev@gmail.com>
2022-08-08 15:49:02 -0300

    finalizado a API poupadev

--- PoupaDev.API/Controllers/ObjetivosFinanceirosController.cs
+++ PoupaDev.API/Controllers/ObjetivosFinanceirosController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PoupaDev.API.Entities;
 using PoupaDev.API.Models;
+using PoupaDev.API.Persistence;
 
 namespace PoupaDev.API.Controllers
 {
@@ -11,29 +13,61 @@ namespace PoupaDev.API.Controllers
     [Route("api/objetivos-financeiros")]
     public class ObjetivosFinanceirosController : ControllerBase
     {
+        private readonly PoupaDevContext _context;
+        public ObjetivosFinanceirosController(PoupaDevContext context)
+        {
+            _context = context;
+        }
         // GET api/objetivos-financeiros
         [HttpGet]
         public IActionResult GetTodos() {
-            return Ok();
+            var objetivos = _context.Objetivos;
+            
+            return Ok(objetivos);
         }
 
         // GET api/objetivos-financeiros/1
         [HttpGet("{id}")]
         public IActionResult GetPorId(int id) {
             // se não achar, retornar NotFound();
-            return Ok();
+            var objetivo = _context.Objetivos.SingleOrDefault(o => o.Id == id);
+
+            if (objetivo == null) {
+                return NotFound();
+            }
+
+            return Ok(objetivo);
         }
 
         // POST api/objetivos-financeiros
         [HttpPost]
         public IActionResult Post(ObjetivoFinanceiroInputModel model) {
             // Se dados de entrada estiverem invalidos, retornar BadRequest();
-            return Ok();        
+            var objetivo = new ObjetivoFinanceiro(model.Titulo, model.Descricao, model.ValorObjetivo);
+
+            _context.Objetivos.Add(objetivo);
+
+            var id = objetivo.Id;
+            return CreatedAtAction(
+                "GetPorId",
+                new {id = id},
+                model
+                );        
         }
 
         // POST api/objetivos-financeiros/1/operacoes
         [HttpPost("{id}/operacoes")]
         public IActionResult PostOperacao(int id, OperacaoInputModel model) {
+            var operacao = new Operacao(model.Valor, model.TipoOperacao);
+
+            var objetivo = _context.Objetivos.SingleOrDefault(o => o.Id == id);
+
+            if (objetivo == null) {
+                return NotFound();
+            }
+
+            objetivo.RealizarOperacao(operacao);
+
             return NoContent();
         }
     }
